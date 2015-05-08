220e72f7e43bdb8e5658460e8fcc877e717e2adf
gaye <gaye@mozilla.com>
2015-05-08 14:14:59 -0400

    Make basic_test.js actually navigate to GH

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,9 @@
+if [ -z "$RUNTIME" ]; then
+  RUNTIME=/Applications/Firefox.app/Contents/MacOS/firefox-bin
+fi
+
 node_modules/.bin/marionette-mocha \
   --host marionette-firefox-host \
-  --runtime /Applications/Firefox.app/Contents/MacOS/firefox-bin \
+  --runtime $RUNTIME \
+  --timeout 60s \
   tests/basic.js
