a019c21776dd31d06c5051cf0e6970840b0559d6
Vlad Filippov <vlad.filippov@gmail.com>
2015-05-08 12:36:25 -0400

    use stable

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 node_modules/.bin/marionette-mocha \
   --host marionette-firefox-host \
-  --runtime /Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin \
+  --runtime /Applications/Firefox.app/Contents/MacOS/firefox-bin \
   tests/basic.js
