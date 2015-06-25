a5a182922df71b0516d755ee80067787a3a65e76
Peter deHaan <peter@deseloper.com>
2015-06-25 14:30:28 -0700

    loop signup guessing

--- tests/loop_signup.js
+++ tests/loop_signup.js
@@ -2,8 +2,6 @@ var Promise = require('promise');
 var expect = require('chai').expect;
 marionette.plugin('helper', require('marionette-helper'));
 marionette('loop', function() {
-
-
   var client = marionette.client({
     prefs: {
       'browser.shell.checkDefaultBrowser': false,
@@ -37,6 +35,32 @@ marionette('loop', function() {
       .findElement(':root')
       .findElement('#loop-button')
       .click()
+
+    //client
+    //  .findElement('#loop-notification-panel')
+
+    client.waitFor(function () {
+      return client.findElement('#loop-button')
+      // client.findElement('#fte-button');
+    });
+
+    client.waitFor(function () {
+      return client.findElement('#loop-notification-panel');
+    });
+
+    var loopDoc = client.findElement('#loop-panel-iframe');//.children[0].contentDocument;
+
+    console.log(loopDoc);
+
+    // client.switchToFrame() // 'loop-panel-iframe'); //, {focus: true})
+    // console.log(client.pageSource())
+
+    client.waitFor(function () {
+      return client.findElement('#fte-button');
+    });
+
+      //.findElement('#fte-button')
+      //.click()
       //.switchToFrame('chat-frame')
 
     console.log('clicked loop button');
